73cec90ce5da3f5294231306ab35e1c0b07ff9b4
Gadget Mobile <the_gadget_mobile@yahoo.com>
2020-03-24 17:29:15 +0100

    add platform setup tests in every class

--- tests/components/blebox/test_cover.py
+++ tests/components/blebox/test_cover.py
@@ -19,7 +19,7 @@ from homeassistant.components.cover import (
     SUPPORT_STOP,
 )
 
-from .conftest import DefaultBoxTest, mock_config, mock_feature
+from .conftest import DefaultBoxTest, mock_feature
 
 
 class CoverTest(DefaultBoxTest):
@@ -148,17 +148,6 @@ class CoverTest(DefaultBoxTest):
         self.assert_state(entity, STATE_OPENING)
         await entity.async_stop_cover()
         self.assert_state(entity, STATE_OPEN)
-
-    async def test_async_setup_platform(self, hass):
-        """Test async_setup_platform (for coverage)."""
-        config = mock_config("172.90.80.70")
-        config.add_to_hass(hass)
-
-        def async_add(entities, state):
-            assert state is True
-            assert len(entities) == 1
-
-        assert await cover.async_setup_platform(hass, config.data, async_add)
 
 
 class TestShutter(CoverTest):
